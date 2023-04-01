364549a11d56902154a51baaa3c40dcc997a2c63
Suranjan <suranjandesilva1995@gmail.com>
2023-04-01 23:56:55 +0530

    initial stage with only HTML & CSS

---
+++ cv.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>My CV</title>
+</head>
+<body>
+    <div class="main">
+        <div class="sidesection">
+            <!-- main photo -->
+            <div>
+                <img src="img/CV Photo.jpg" alt="" class="center" width="150px" height="150px">
+            </div>
+            <!-- my contact details -->
+            <div>
+                <div class="div-icons">
+                    <img src="img/insta.png" class="icons" alt=""> 
+                </div>
+                <div>
+                    No 633, Galle Road, 
+                    Randombe, Ambalangoda.
+                </div> <br>
+
+                <div class="div-icons">
+                    <img src="img/insta.png" class="icons" alt="">  
+                </div>
+                <div>(+94) 76 9852502</div><br><br>
+
+                <div class="div-icons">
+                    <img src="img/whatsapp.png" class="icons" alt=""> 
+                </div> 
+                <div>suranjandesilva1995 <br> @gmail.com</div> <br> 
+
+                <div class="div-icons">
+                    <img src="img/in.png" class="icons" alt=""> 
+                </div>
+                <div>www.linkedin.com/ <br> in/suranjan-de-silva </div>
+            </div>
+            <!-- Working Experiences -->
+            <div class="work">
+                <h3 class="sub-topic">Working Experiences</h3>
+                <div class="work1">
+                    <ul> <li><b>2022 July – 2023 January:</b>  IE 
+                    Internship at MAS intimates 
+                    Unichela Koggala.</li> </ul> 
+                </div>
+                <div class="work2">
+                    <ul><li><b>2017 January – 2017 July:</b>  
+                    Completed “job skills 
+                    Development Training 
+                    Programme for School 
+                    leavers” at Bank of Ceylon, 
+                    Rathgama Branch.</li> </ul> 
+                </div>
+            </div>
+            <!-- Skills -->
+            <div class="skills">
+                <h3 class="sub-topic">Skills</h3>
+                <div class="skill-1">
+                    <div>
+                        <h4>Java</h4>
+                    </div>
+                </div>
+                <div class="skill-2">
+                    <div>
+                        <h4>HTML</h4>
+                    </div>
+                </div>
+                <div class="skill-3">
+                    <div>
+                        <h4>CSS</h4>
+                    </div>
+                </div>
+                <div class="skill-4">
+                    <div>
+                        <h4>Javascript</h4>
+                    </div>
+                </div>
+                <div class="skill-5">
+                    <div>
+                        <h4>PHP</h4>
+                    </div>
+                </div>
+                <div class="skill-6">
+                    <div>
+                        <h4>SQL</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- Refrees -->
+            <div class="refrees">
+            <h3 class="sub-topic">Refrees</h3>
+                <div class="ref-1">
+                    <b> Dr. E.A.C.P. Karunarathne</b><br> 
+                    Head, <br>
+                    Department of Industrial 
+                    Management, 
+                    Faculty of Applied Sciences,
+                    Wayamba University of Sri Lanka,
+                    Kuliyapitiya 60200, Sri Lanka.
+                    General: +94 37 2281 412 / 14, 
+                    Office / Fax: +94 37 2283 619 
+                    Mobile: +94 71 857 4400 
+                    Email: chami@wyb.ac.lk
+                </div><br>
+                <div class="ref-2">
+                    <b>Mr. Gihan Gamage </b> 
+                    Manager,
+                    Bank of Ceylon,
+                    Bentota,
+                    Mobile: (+94) 77 381 2794
+                    Email: 
+                    gamage.gihanboc@gmail.com
+                </div><br>
+            </div>
+        </div>
+        <!-- End of the sidebar -->
+        <!-- begining of the main-content part -->
+        <div class="main-content">
+            <!-- profile -->
+            <div>
+              <div class="name"> <h1>Suranjan De Silva</h1> </div>  
+              <div class="profile">
+              A detail-oriented and highly organized undergraduate reading for Bachelors in Industrial Management and Computer Management & Information systems. Seeking a web application-based profile in your company to gain practical exposure along with implement my knowledge and expertise to ensure the success of the organizational goals.
+              </div>
+            </div>
+            <!-- Educational Qualification -->
+            <div>
+                <div>
+                    <h3 class="sub-topic">Educational Qualification</h3>
+                </div>  
+                <div>
+                    <ul>
+                         <li><b>2018 to 2023:</b>  Wayamba University of Sri Lanka,
+                        B.Sc. (Joint Major) Industrial Management and Computer 
+                        Management and Information Systems.</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                         <li><b>2017 July:</b>  ESOFT Metro Campus, DiTEC – Diploma 
+                        in Information Technology with E-commerce 
+                        (International), Won Medal for “Best student of the intake”</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                         <li><b>2021 August:</b>2021 August: Apex Institute of Management 
+                        Services, Certificate Program industrial Engineering and 
+                        work study fundamentals</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><b>2022 January:</b>  Business Excellence Institute, Six 
+                        Sigma yellow belt. (Online)</li> 
+                    </ul>
+                </div> 
+            </div>
+            <!-- Projects at University -->
+            <div>
+                <div>
+                    <h3 class="sub-topic">Projects at University</h3>
+                </div>  
+                <div>
+                    <ul>
+                     <li>Computer Based Modeling & Simulation Group 
+                    Research: Analysis of One day passport issuing service 
+                    performance in the Department of Immigration and 
+                    Emigration Sri Lanka using the Arena simulation model. 
+                    Jan 2022 – May 2022.</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Environmental Management System Based on 
+                    ISO 14001 Group Research: Environmental impact of 
+                    Household polythene usage and the Disposal 
+                    mechanisms in Sri Lanka: Considering Galle district. Jan 
+                    2022 – May 2022</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Design and Development of Computer based 
+                    project: Inventory & Sales Management System for 
+                    Chenith Enterprises (pvt) Ltd. April 2020 – April 2021.</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Web Designing & E-Commerce Group project:
+                    Developed Online Recipes System. November 2020 –
+                    April 2021</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Software Engineering Group project:
+                    Group leader of developing Online cake Ordering System 
+                    for cake shop. November 2020 – April 2021.</li>  
+                    </ul>
+                </div> 
+                <div>
+                    <ul>
+                    <li>Rapid Application Development Project:
+                    Online Salon Management System for Salon JISELLE
+                    Hikkaduwa. April 2020 – November 2020.</li>  
+                    </ul>
+                </div>  
+            </div>
+            <!-- Certification and volunteering -->
+            <div>
+                <div>
+                    <h3 class="sub-topic">Certification and Volunteering</h3>
+                </div>  
+                <div>
+                    <ul>
+                     <li>Member of “Wayamba Ahasa Yata 2018” Organizing Committee</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Player of University Badminton team in SLUG 2019.</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                    <li>Volunteer at Mathematical Sciences Day 2019 
+                    organized by Mathematical society of Wayamba.</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Active Member at “Wayoscope”. (Media Unit of Wayamba University)</li> 
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                     <li>Contributed to Wayoscope Media Team at 11th ASBIRES (Applied Sciences, Business & Research Symposium).</li>  
+                    </ul>
+                </div>
+                <!-- Declaration -->
+                <div class="declaration">
+                    I hereby declare that the above information given is true and accurate to the best of my knowledge. 
+                </div>
+                <div class="date">
+                    ..................... <br>
+                    Date
+                </div>
+                <div class="sign">
+                    ..................... <br>
+                    Signature
+                </div>
+                            
+            </div>
+                
+        </div>
+    </div>
+</body>
+</html>
